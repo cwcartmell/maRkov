25e96a7cbeefca4b41a6d4b929ff884865c06d00
cwcartmell <cwcartmell@gmail.com>
2016-05-02 14:20:48 -0700

    Made methods consistent with their generics for CRAN compliance.

--- R/multiple.binary.test_methods.R
+++ R/multiple.binary.test_methods.R
@@ -3,13 +3,14 @@
 #' Instruct user to use \code{summary} and \code{print} to examine objects of
 #' \link{class} \code{multiple.binary.test}.
 #'
-#' @param multiple.binary.test An object of \code{\link{class}}
+#' @param x An object of \code{\link{class}}
 #' \code{multiple.binary.test}.
+#' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
-print.multiple.binary.test <- function(multiple.binary.test) {
+print.multiple.binary.test <- function(x, ...) {
     cat("\ncall:\n")
-    print(multiple.binary.test$call)
+    print(x$call)
     cat("\n---\nUse summary() and plot() for more information.")
 }
 
@@ -18,68 +19,69 @@ print.multiple.binary.test <- function(multiple.binary.test) {
 #' Prints test statistics and p-values for objects of \link{class}
 #' \code{multiple.binary.test}.
 #'
-#' @param multiple.binary.test An object of \code{\link{class}}
+#' @param object An object of \code{\link{class}}
 #' \code{multiple.binary.test}.
+#' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
-summary.multiple.binary.test <- function(multiple.binary.test) {
-    cat("\ncall:\n")
-    print(multiple.binary.test$call)
-    cat("\nTest statistics\n")
-    cat("                 Min          1Q      Median          3Q         Max   test stat  Pr(>test stat)\n")
-    cat("LRT\t", format(quantile(multiple.binary.test$test.stats.lrt)[1], trim = FALSE, justify = "right", width = 10), format(quantile(multiple.binary.test$test.stats.lrt)[2],
-        trim = FALSE, justify = "right", width = 10), format(quantile(multiple.binary.test$test.stats.lrt)[3], trim = FALSE, justify = "right",
-        width = 10), format(quantile(multiple.binary.test$test.stats.lrt)[4], trim = FALSE, justify = "right", width = 10), format(quantile(multiple.binary.test$test.stats.lrt)[5],
-        trim = FALSE, justify = "right", width = 10), format(multiple.binary.test$test.stats.lrt[1], trim = FALSE, justify = "right", width = 10),
-        format(multiple.binary.test$p.value.lrt, trim = FALSE, justify = "right", width = 10), sep = "  ")
-    if (multiple.binary.test$p.value.lrt < 0.001) {
-        cat(" ***\n")
-    } else if (multiple.binary.test$p.value.lrt < 0.01 & multiple.binary.test$p.value.lrt >= 0.001) {
-        cat(" **\n")
-    } else if (multiple.binary.test$p.value.lrt < 0.05 & multiple.binary.test$p.value.lrt >= 0.01) {
-        cat(" *\n")
-    } else if (multiple.binary.test$p.value.lrt < 0.1 & multiple.binary.test$p.value.lrt >= 0.05) {
-        cat(" .\n")
-    } else if (multiple.binary.test$p.value.lrt < 1 & multiple.binary.test$p.value.lrt >= 0.1) {
-        cat(" \n")
-    }
+summary.multiple.binary.test <- function(object, ...) {
+  cat("\ncall:\n")
+  print(object$call)
+  cat("\nTest statistics\n")
+  cat("                 Min          1Q      Median          3Q         Max   test stat  Pr(>test stat)\n")
+  cat("LRT\t", format(quantile(object$test.stats.lrt)[1], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.lrt)[2],
+                                                                                                               trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.lrt)[3], trim = FALSE, justify = "right",
+                                                                                                                                                                    width = 10), format(quantile(object$test.stats.lrt)[4], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.lrt)[5],
+                                                                                                                                                                                                                                                                                 trim = FALSE, justify = "right", width = 10), format(object$test.stats.lrt[1], trim = FALSE, justify = "right", width = 10),
+      format(object$p.value.lrt, trim = FALSE, justify = "right", width = 10), sep = "  ")
+  if (object$p.value.lrt < 0.001) {
+    cat(" ***\n")
+  } else if (object$p.value.lrt < 0.01 & object$p.value.lrt >= 0.001) {
+    cat(" **\n")
+  } else if (object$p.value.lrt < 0.05 & object$p.value.lrt >= 0.01) {
+    cat(" *\n")
+  } else if (object$p.value.lrt < 0.1 & object$p.value.lrt >= 0.05) {
+    cat(" .\n")
+  } else if (object$p.value.lrt < 1 & object$p.value.lrt >= 0.1) {
+    cat(" \n")
+  }
 
-    cat("ChiSq\t", format(quantile(multiple.binary.test$test.stats.chisq), trim = FALSE, justify = "right", width = 10)[1], format(quantile(multiple.binary.test$test.stats.chisq)[2],
-        trim = FALSE, justify = "right", width = 10), format(quantile(multiple.binary.test$test.stats.chisq)[3], trim = FALSE, justify = "right",
-        width = 10), format(quantile(multiple.binary.test$test.stats.chisq)[4], trim = FALSE, justify = "right", width = 10), format(quantile(multiple.binary.test$test.stats.chisq)[5],
-        trim = FALSE, justify = "right", width = 10), format(multiple.binary.test$test.stats.chisq[1], trim = FALSE, justify = "right", width = 10),
-        format(multiple.binary.test$p.value.chisq, trim = FALSE, justify = "right", width = 10), sep = "  ")
-    if (multiple.binary.test$p.value.chisq < 0.001) {
-        cat(" ***\n")
-    } else if (multiple.binary.test$p.value.chisq < 0.01 & multiple.binary.test$p.value.chisq >= 0.001) {
-        cat(" **\n")
-    } else if (multiple.binary.test$p.value.chisq < 0.05 & multiple.binary.test$p.value.chisq >= 0.01) {
-        cat(" *\n")
-    } else if (multiple.binary.test$p.value.chisq < 0.1 & multiple.binary.test$p.value.chisq >= 0.05) {
-        cat(" .\n")
-    } else if (multiple.binary.test$p.value.chisq < 1 & multiple.binary.test$p.value.chisq >= 0.1) {
-        cat(" \n")
-    }
+  cat("ChiSq\t", format(quantile(object$test.stats.chisq), trim = FALSE, justify = "right", width = 10)[1], format(quantile(object$test.stats.chisq)[2],
+                                                                                                                   trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.chisq)[3], trim = FALSE, justify = "right",
+                                                                                                                                                                        width = 10), format(quantile(object$test.stats.chisq)[4], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.chisq)[5],
+                                                                                                                                                                                                                                                                                       trim = FALSE, justify = "right", width = 10), format(object$test.stats.chisq[1], trim = FALSE, justify = "right", width = 10),
+      format(object$p.value.chisq, trim = FALSE, justify = "right", width = 10), sep = "  ")
+  if (object$p.value.chisq < 0.001) {
+    cat(" ***\n")
+  } else if (object$p.value.chisq < 0.01 & object$p.value.chisq >= 0.001) {
+    cat(" **\n")
+  } else if (object$p.value.chisq < 0.05 & object$p.value.chisq >= 0.01) {
+    cat(" *\n")
+  } else if (object$p.value.chisq < 0.1 & object$p.value.chisq >= 0.05) {
+    cat(" .\n")
+  } else if (object$p.value.chisq < 1 & object$p.value.chisq >= 0.1) {
+    cat(" \n")
+  }
 
-    cat("Run =", multiple.binary.test$run, format(quantile(multiple.binary.test$test.stats.run)[1], trim = FALSE, justify = "right", width = 10),
-        format(quantile(multiple.binary.test$test.stats.run)[2], trim = FALSE, justify = "right", width = 10), format(quantile(multiple.binary.test$test.stats.run)[3],
-            trim = FALSE, justify = "right", width = 10), format(quantile(multiple.binary.test$test.stats.run)[4], trim = FALSE, justify = "right",
-            width = 10), format(quantile(multiple.binary.test$test.stats.run)[5], trim = FALSE, justify = "right", width = 10), format(multiple.binary.test$test.stats.run[1],
-            trim = FALSE, justify = "right", width = 10), format(multiple.binary.test$p.value.run, trim = FALSE, justify = "right", width = 10),
-        sep = "  ")
-    if (multiple.binary.test$p.value.run < 0.001) {
-        cat(" ***\n")
-    } else if (multiple.binary.test$p.value.run < 0.01 & multiple.binary.test$p.value.run >= 0.001) {
-        cat(" **\n")
-    } else if (multiple.binary.test$p.value.run < 0.05 & multiple.binary.test$p.value.run >= 0.01) {
-        cat(" *\n")
-    } else if (multiple.binary.test$p.value.run < 0.1 & multiple.binary.test$p.value.run >= 0.05) {
-        cat(" .\n")
-    } else if (multiple.binary.test$p.value.run < 1 & multiple.binary.test$p.value.run >= 0.1) {
-        cat(" \n")
-    }
+  cat("Run =", object$run, format(quantile(object$test.stats.run)[1], trim = FALSE, justify = "right", width = 10),
+      format(quantile(object$test.stats.run)[2], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.run)[3],
+                                                                                                      trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.run)[4], trim = FALSE, justify = "right",
+                                                                                                                                                           width = 10), format(quantile(object$test.stats.run)[5], trim = FALSE, justify = "right", width = 10), format(object$test.stats.run[1],
+                                                                                                                                                                                                                                                                        trim = FALSE, justify = "right", width = 10), format(object$p.value.run, trim = FALSE, justify = "right", width = 10),
+      sep = "  ")
+  if (object$p.value.run < 0.001) {
+    cat(" ***\n")
+  } else if (object$p.value.run < 0.01 & object$p.value.run >= 0.001) {
+    cat(" **\n")
+  } else if (object$p.value.run < 0.05 & object$p.value.run >= 0.01) {
+    cat(" *\n")
+  } else if (object$p.value.run < 0.1 & object$p.value.run >= 0.05) {
+    cat(" .\n")
+  } else if (object$p.value.run < 1 & object$p.value.run >= 0.1) {
+    cat(" \n")
+  }
 
-    cat("---\nSignif. codes: 0 '***' 0.001 '**' 0.01 '*' .05 '.' .1 ' ' 1\n---\n")
+  cat("---\nSignif. codes: 0 '***' 0.001 '**' 0.01 '*' .05 '.' .1 ' ' 1\n---\n")
 }
 
 #' Produce plots from objects of class "multiple.binary.test".
@@ -87,28 +89,29 @@ summary.multiple.binary.test <- function(multiple.binary.test) {
 #' Produces some interesting plots of data and test statistics of objects of
 #' \link{class} \code{multiple.binary.test}.
 #'
-#' @param multiple.binary.test An object of \code{\link{class}}
+#' @param x An object of \code{\link{class}}
 #' \code{multiple.binary.test}.
+#' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
-plot.multiple.binary.test <- function(multiple.binary.test) {
-    par(ask = TRUE,  mar = c(5, 1, 1, 1))
-    x <- multiple.binary.test$data[[1]]
-    reverse <- nrow(x):1
-    x <- x[reverse, ]
-    image(c(1:ncol(x)), c(1:nrow(x)), t(x), axes = FALSE, ylab = " ", xlab = "Visual representation of the actual Markov Chain.", col = c("aquamarine3",
-        "chocolate1"))
+plot.multiple.binary.test <- function(x, ...) {
+  par(ask = TRUE,  mar = c(5, 1, 1, 1))
+  y <- x$data[[1]]
+  reverse <- nrow(y):1
+  y <- y[reverse, ]
+  image(c(1:ncol(y)), c(1:nrow(y)), t(y), axes = FALSE, ylab = " ", xlab = "Visual representation of the actual Markov Chain.", col = c("aquamarine3",
+                                                                                                                                        "chocolate1"))
 
-    par(ask = TRUE, mar = c(5, 4, 4, 2))
-    hist(multiple.binary.test$test.stats.lrt, breaks = multiple.binary.test$bins, xlab = "LRT Test Statistics\nRed line indicates value of test statistic of real data",
-        main = NULL)
-    abline(v = multiple.binary.test$test.stats.lrt[1], col = "red")
+  par(ask = TRUE, mar = c(5, 4, 4, 2))
+  hist(x$test.stats.lrt, breaks = x$bins, xlab = "LRT Test Statistics\nRed line indicates value of test statistic of real data",
+       main = NULL)
+  abline(v = x$test.stats.lrt[1], col = "red")
 
-    hist(multiple.binary.test$test.stats.chisq, breaks = multiple.binary.test$bins, xlab = "Pearson's Chi-Square Test Statistics\nRed line indicates value of test statistic of real data",
-        main = NULL)
-    abline(v = multiple.binary.test$test.stats.chisq[1], col = "red")
+  hist(x$test.stats.chisq, breaks = x$bins, xlab = "Pearson's Chi-Square Test Statistics\nRed line indicates value of test statistic of real data",
+       main = NULL)
+  abline(v = x$test.stats.chisq[1], col = "red")
 
-    hist(multiple.binary.test$test.stats.run, breaks = multiple.binary.test$bins, xlab = paste("Run Test Statistics For a Run of", multiple.binary.test$run,
-        "\nRed line indicates value of test statistic of real data"), main = NULL)
-    abline(v = multiple.binary.test$test.stats.run[1], col = "red")
+  hist(x$test.stats.run, breaks = x$bins, xlab = paste("Run Test Statistics For a Run of", x$run,
+                                                       "\nRed line indicates value of test statistic of real data"), main = NULL)
+  abline(v = x$test.stats.run[1], col = "red")
 }
--- R/single.binary.test_methods.R
+++ R/single.binary.test_methods.R
@@ -3,13 +3,14 @@
 #' Instruct user to use \code{summary} and \code{print} to examine objects of
 #' \link{class} \code{single.binary.test}.
 #'
-#' @param single.binary.test An object of \code{\link{class}}
+#' @param x An object of \code{\link{class}}
 #' \code{single.binary.test}.
+#' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
-print.single.binary.test <- function(single.binary.test) {
+print.single.binary.test <- function(x, ...) {
     cat("\nCall:\n")
-    print(single.binary.test$call)
+    print(x$call)
     cat("\n---\nUse summary() and plot() for more information.")
 }
 
@@ -18,73 +19,74 @@ print.single.binary.test <- function(single.binary.test) {
 #' Prints test statistics, p-values, and provides sample data for objects of
 #' \link{class} \code{single.binary.test}.
 #'
-#' @param single.binary.test An object of \code{\link{class}}
+#' @param object An object of \code{\link{class}}
 #' \code{single.binary.test}.
+#' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
-summary.single.binary.test <- function(single.binary.test) {
-    cat("\nCall:\n")
-    print(single.binary.test$call)
-    cat("\nVarious test statistics:\n")
-    cat("                 Min          1Q      Median          3Q         Max   test stat  Pr(>test stat)\n")
-    cat("LRT\t", format(quantile(single.binary.test$test.stats.lrt)[1], trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.lrt)[2],
-        trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.lrt)[3], trim = FALSE, justify = "right", width = 10),
-        format(quantile(single.binary.test$test.stats.lrt)[4], trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.lrt)[5],
-            trim = FALSE, justify = "right", width = 10), format(single.binary.test$test.stats.lrt[1], trim = FALSE, justify = "right", width = 10),
-        format(single.binary.test$p.value.lrt, trim = FALSE, justify = "right", width = 10), sep = "  ")
-    if (single.binary.test$p.value.lrt < 0.001) {
-        cat(" ***\n")
-    } else if (single.binary.test$p.value.lrt < 0.01 & single.binary.test$p.value.lrt >= 0.001) {
-        cat(" **\n")
-    } else if (single.binary.test$p.value.lrt < 0.05 & single.binary.test$p.value.lrt >= 0.01) {
-        cat(" *\n")
-    } else if (single.binary.test$p.value.lrt < 0.1 & single.binary.test$p.value.lrt >= 0.05) {
-        cat(" .\n")
-    } else if (single.binary.test$p.value.lrt < 1 & single.binary.test$p.value.lrt >= 0.1) {
-        cat(" \n")
-    }
+summary.single.binary.test <- function(object, ...) {
+  cat("\nCall:\n")
+  print(object$call)
+  cat("\nVarious test statistics:\n")
+  cat("                 Min          1Q      Median          3Q         Max   test stat  Pr(>test stat)\n")
+  cat("LRT\t", format(quantile(object$test.stats.lrt)[1], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.lrt)[2],
+                                                                                                               trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.lrt)[3], trim = FALSE, justify = "right", width = 10),
+      format(quantile(object$test.stats.lrt)[4], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.lrt)[5],
+                                                                                                      trim = FALSE, justify = "right", width = 10), format(object$test.stats.lrt[1], trim = FALSE, justify = "right", width = 10),
+      format(object$p.value.lrt, trim = FALSE, justify = "right", width = 10), sep = "  ")
+  if (object$p.value.lrt < 0.001) {
+    cat(" ***\n")
+  } else if (object$p.value.lrt < 0.01 & object$p.value.lrt >= 0.001) {
+    cat(" **\n")
+  } else if (object$p.value.lrt < 0.05 & object$p.value.lrt >= 0.01) {
+    cat(" *\n")
+  } else if (object$p.value.lrt < 0.1 & object$p.value.lrt >= 0.05) {
+    cat(" .\n")
+  } else if (object$p.value.lrt < 1 & object$p.value.lrt >= 0.1) {
+    cat(" \n")
+  }
 
-    cat("ChiSq\t", format(quantile(single.binary.test$test.stats.chisq), trim = FALSE, justify = "right", width = 10)[1], format(quantile(single.binary.test$test.stats.chisq)[2],
-        trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.chisq)[3], trim = FALSE, justify = "right",
-        width = 10), format(quantile(single.binary.test$test.stats.chisq)[4], trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.chisq)[5],
-        trim = FALSE, justify = "right", width = 10), format(single.binary.test$test.stats.chisq[1], trim = FALSE, justify = "right", width = 10),
-        format(single.binary.test$p.value.chisq, trim = FALSE, justify = "right", width = 10), sep = "  ")
-    if (single.binary.test$p.value.chisq < 0.001) {
-        cat(" ***\n")
-    } else if (single.binary.test$p.value.chisq < 0.01 & single.binary.test$p.value.chisq >= 0.001) {
-        cat(" **\n")
-    } else if (single.binary.test$p.value.chisq < 0.05 & single.binary.test$p.value.chisq >= 0.01) {
-        cat(" *\n")
-    } else if (single.binary.test$p.value.chisq < 0.1 & single.binary.test$p.value.chisq >= 0.05) {
-        cat(" .\n")
-    } else if (single.binary.test$p.value.chisq < 1 & single.binary.test$p.value.chisq >= 0.1) {
-        cat(" \n")
-    }
+  cat("ChiSq\t", format(quantile(object$test.stats.chisq), trim = FALSE, justify = "right", width = 10)[1], format(quantile(object$test.stats.chisq)[2],
+                                                                                                                   trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.chisq)[3], trim = FALSE, justify = "right",
+                                                                                                                                                                        width = 10), format(quantile(object$test.stats.chisq)[4], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.chisq)[5],
+                                                                                                                                                                                                                                                                                       trim = FALSE, justify = "right", width = 10), format(object$test.stats.chisq[1], trim = FALSE, justify = "right", width = 10),
+      format(object$p.value.chisq, trim = FALSE, justify = "right", width = 10), sep = "  ")
+  if (object$p.value.chisq < 0.001) {
+    cat(" ***\n")
+  } else if (object$p.value.chisq < 0.01 & object$p.value.chisq >= 0.001) {
+    cat(" **\n")
+  } else if (object$p.value.chisq < 0.05 & object$p.value.chisq >= 0.01) {
+    cat(" *\n")
+  } else if (object$p.value.chisq < 0.1 & object$p.value.chisq >= 0.05) {
+    cat(" .\n")
+  } else if (object$p.value.chisq < 1 & object$p.value.chisq >= 0.1) {
+    cat(" \n")
+  }
 
-    cat("Run =", single.binary.test$run, format(quantile(single.binary.test$test.stats.run)[1], trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.run)[2],
-        trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.run)[3], trim = FALSE, justify = "right", width = 10),
-        format(quantile(single.binary.test$test.stats.run)[4], trim = FALSE, justify = "right", width = 10), format(quantile(single.binary.test$test.stats.run)[5],
-            trim = FALSE, justify = "right", width = 10), format(single.binary.test$test.stats.run[1], trim = FALSE, justify = "right", width = 10),
-        format(single.binary.test$p.value.run, trim = FALSE, justify = "right", width = 10), sep = "  ")
-    if (single.binary.test$p.value.run < 0.001) {
-        cat(" ***\n")
-    } else if (single.binary.test$p.value.run < 0.01 & single.binary.test$p.value.run >= 0.001) {
-        cat(" **\n")
-    } else if (single.binary.test$p.value.run < 0.05 & single.binary.test$p.value.run >= 0.01) {
-        cat(" *\n")
-    } else if (single.binary.test$p.value.run < 0.1 & single.binary.test$p.value.run >= 0.05) {
-        cat(" .\n")
-    } else if (single.binary.test$p.value.run < 1 & single.binary.test$p.value.run >= 0.1) {
-        cat(" \n")
-    }
+  cat("Run =", object$run, format(quantile(object$test.stats.run)[1], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.run)[2],
+                                                                                                                           trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.run)[3], trim = FALSE, justify = "right", width = 10),
+      format(quantile(object$test.stats.run)[4], trim = FALSE, justify = "right", width = 10), format(quantile(object$test.stats.run)[5],
+                                                                                                      trim = FALSE, justify = "right", width = 10), format(object$test.stats.run[1], trim = FALSE, justify = "right", width = 10),
+      format(object$p.value.run, trim = FALSE, justify = "right", width = 10), sep = "  ")
+  if (object$p.value.run < 0.001) {
+    cat(" ***\n")
+  } else if (object$p.value.run < 0.01 & object$p.value.run >= 0.001) {
+    cat(" **\n")
+  } else if (object$p.value.run < 0.05 & object$p.value.run >= 0.01) {
+    cat(" *\n")
+  } else if (object$p.value.run < 0.1 & object$p.value.run >= 0.05) {
+    cat(" .\n")
+  } else if (object$p.value.run < 1 & object$p.value.run >= 0.1) {
+    cat(" \n")
+  }
 
-    cat("---\nSignif. codes: 0 '***' 0.001 '**' 0.01 '*' .05 '.' .1 ' ' 1\n---\n")
-    cat("Quick sample of real and generated data:\n")
-    cat("       Real data :\t\t", single.binary.test$data[1, ], "\n")
-    cat("Generated data 1 :\t\t", single.binary.test$data[2, ], "\n")
-    cat("Generated data 2 :\t\t", single.binary.test$data[3, ], "\n")
-    cat(".\n.\n.\n")
-    cat("Generated data", length(single.binary.test$data[, 1]) - 1, ":\t\t", single.binary.test$data[length(single.binary.test$data[, 1]), ], "\n")
+  cat("---\nSignif. codes: 0 '***' 0.001 '**' 0.01 '*' .05 '.' .1 ' ' 1\n---\n")
+  cat("Quick sample of real and generated data:\n")
+  cat("       Real data :\t\t", object$data[1, ], "\n")
+  cat("Generated data 1 :\t\t", object$data[2, ], "\n")
+  cat("Generated data 2 :\t\t", object$data[3, ], "\n")
+  cat(".\n.\n.\n")
+  cat("Generated data", length(object$data[, 1]) - 1, ":\t\t", object$data[length(object$data[, 1]), ], "\n")
 }
 
 #' Produce plots from objects of class "single.binary.test".
@@ -92,32 +94,32 @@ summary.single.binary.test <- function(single.binary.test) {
 #' Produces some interesting plots of data and test statistics of objects of
 #' \link{class} \code{single.binary.test}.
 #'
-#' @param single.binary.test An object of \code{\link{class}}
+#' @param x An object of \code{\link{class}}
 #' \code{single.binary.test}.
+#' @param ... Further arguments passed to or from other methods.
 #'
 #' @export
-plot.single.binary.test <- function(single.binary.test) {
-    par(ask = TRUE, mar = c(5, 1, 1, 1))
-    x <- single.binary.test$data
-    x <- x[1:single.binary.test$tiles, ]
-    reverse <- nrow(x):1
-    # yLabels <- yLabels[reverse]
-    x <- x[reverse, ]
-    image(c(1:ncol(x)), c(1:nrow(x)), t(x), axes = FALSE, ylab = " ", xlab = "Real data above the black line, generated data below it", col = c("aquamarine3",
-        "chocolate1"))
-    abline(h = single.binary.test$tiles - 0.5, col = "black", lwd = 4)
+plot.single.binary.test <- function(x, ...) {
+  par(ask = TRUE, mar = c(5, 1, 1, 1))
+  y <- x$data
+  y <- y[1:x$tiles, ]
+  reverse <- nrow(y):1
+  y <- y[reverse, ]
+  image(c(1:ncol(y)), c(1:nrow(y)), t(y), axes = FALSE, ylab = " ", xlab = "Real data above the black line, generated data below it", col = c("aquamarine3",
+                                                                                                                                              "chocolate1"))
+  abline(h = x$tiles - 0.5, col = "black", lwd = 4)
 
-    par(ask = TRUE, mar = c(5, 4, 4, 2))
+  par(ask = TRUE, mar = c(5, 4, 4, 2))
 
-    hist(single.binary.test$test.stats.lrt, breaks = single.binary.test$bins, xlab = "LRT Test Statistics\nRed line indicates value of test statistic of real data",
-        main = NULL)
-    abline(v = single.binary.test$test.stats.lrt[1], col = "red")
+  hist(x$test.stats.lrt, breaks = x$bins, xlab = "LRT Test Statistics\nRed line indicates value of test statistic of real data",
+       main = NULL)
+  abline(v = x$test.stats.lrt[1], col = "red")
 
-    hist(single.binary.test$test.stats.chisq, breaks = single.binary.test$bins, xlab = "Pearson's Chi-Square Test Statistics\nRed line indicates value of test statistic of real data",
-        main = NULL)
-    abline(v = single.binary.test$test.stats.chisq[1], col = "red")
+  hist(x$test.stats.chisq, breaks = x$bins, xlab = "Pearson's Chi-Square Test Statistics\nRed line indicates value of test statistic of real data",
+       main = NULL)
+  abline(v = x$test.stats.chisq[1], col = "red")
 
-    hist(single.binary.test$test.stats.run, breaks = single.binary.test$bins, xlab = paste("Run Test Statistics For a Run of", single.binary.test$run,
-        "\nRed line indicates value of test statistic of real data"), main = NULL)
-    abline(v = single.binary.test$test.stats.run[1], col = "red")
+  hist(x$test.stats.run, breaks = x$bins, xlab = paste("Run Test Statistics For a Run of", x$run,
+                                                       "\nRed line indicates value of test statistic of real data"), main = NULL)
+  abline(v = x$test.stats.run[1], col = "red")
 }
